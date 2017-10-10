035e36f27c0894e4c147c651d4a0117492d2202b
Roman Hock <hock@crossbow.de>
2017-10-10 23:37:08 +0200

    animation function

--- main.cpp
+++ main.cpp
@@ -12,217 +12,186 @@ float resMultiplierF = resMultiplier;
 
 // Classes
 namespace BRO {
-class Cursor{
-private:
-    sf::Texture cursorTexture;
 
-public:
-    sf::Sprite cursorSprite;
+    class Cursor{
+    private:
+        sf::Texture cursorTexture;
 
-    // Constructor
-    Cursor(){
-        cursorTexture.loadFromFile("cursor.png");
-        cursorSprite.setTexture(cursorTexture);
-        cursorSprite.setOrigin(8, 8);
-        cursorSprite.scale(resMultiplier, resMultiplier);
-        cursorSprite.setPosition(400, 400);
-    }
-    void update(){
-        cursorSprite.setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
-    }
-};
+    public:
+        sf::Sprite cursorSprite;
 
-class Player{
-private:
-    sf::Texture playerTexture;
+        // Constructor
+        Cursor(){
+            cursorTexture.loadFromFile("cursor.png");
+            cursorSprite.setTexture(cursorTexture);
+            cursorSprite.setOrigin(8, 8);
+            cursorSprite.scale(resMultiplier, resMultiplier);
+            cursorSprite.setPosition(400, 400);
+        }
+        void update(){
+            cursorSprite.setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+        }
+    };
 
-    // TODO - fix function - sprite disappears on click
-    void iterateSprite(int top, int startLeft, int maxLeft, int incrementLeft, float speed){
-        playerMask.top = top;
-        if (clock.getElapsedTime().asSeconds() > speed){
-            if (playerMask.left == maxLeft) {
-                playerMask.left = startLeft;
-            }
-            else {
-                playerMask.left += incrementLeft;
-            }
+    class Player{
+    private:
+        sf::Texture playerTexture;
+
+    public:
+        // init Objects
+        sf::IntRect playerMask;
+        sf::Sprite playerSprite;
+        sf::Clock clock;
+        sf::Vector2f moveTarget;
+
+        // Constructor
+        explicit Player(const string &filePath){
+            playerMask.left = 0;
+            playerMask.top = 0;
+            playerMask.width = 64;
+            playerMask.height = 80;
+            playerTexture.loadFromFile(filePath);
+            playerSprite.setTexture(playerTexture);
             playerSprite.setTextureRect(playerMask);
-            clock.restart();
-        }
-    }
-
-public:
-    // init Objects
-    sf::IntRect playerMask;
-    sf::Sprite playerSprite;
-    sf::Clock clock;
-    sf::Vector2f moveTarget;
-
-    // Constructor
-    explicit Player(const string &filePath){
-        playerMask.left = 0;
-        playerMask.top = 0;
-        playerMask.width = 64;
-        playerMask.height = 80;
-        playerTexture.loadFromFile(filePath);
-        playerSprite.setTexture(playerTexture);
-        playerSprite.setTextureRect(playerMask);
-        playerSprite.setOrigin(32, 77);
-        playerSprite.setPosition(200 * resMultiplier, 115 * resMultiplier);
-        playerSprite.scale(resMultiplier, resMultiplier);
-        moveTarget = sf::Vector2f(playerSprite.getPosition());
-    }
-
-    void setTarget(){
-        moveTarget = sf::Vector2f(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
-    }
-
-    void idle(){
-        // TODO - use iterateSprite()
-        playerMask.top = 0;
-        if (clock.getElapsedTime().asSeconds() > 0.2f){
-            if (playerMask.left == 448) {
-                playerMask.left = 0;
-            }
-            else {
-                playerMask.left += 64;
-            }
-            playerSprite.setTextureRect(playerMask);
-            clock.restart();
-        }
-    }
-    void walk(){
-        sf::Vector2f direction = sf::Vector2f(moveTarget.x, moveTarget.y) - playerSprite.getPosition();
-        float magnitude = sqrt((direction.x * direction.x) + (direction.y * direction.y));
-        sf::Vector2f unitVector((direction.x * 1.3f) / magnitude, direction.y / (magnitude * 1.8f));
-
-        // overall player-speed
-        playerSprite.move(unitVector * 0.8f * resMultiplierF);
-
-        float positiveDirectionX;
-        float positiveDirectionY;
-
-        if (direction.x < 0){
-            positiveDirectionX = - direction.x;
-        } else {
-            positiveDirectionX = direction.x;
-        }
-        if (direction.y < 0){
-            positiveDirectionY = - direction.y;
-        } else {
-            positiveDirectionY = direction.y;
+            playerSprite.setOrigin(32, 77);
+            playerSprite.setPosition(200 * resMultiplier, 115 * resMultiplier);
+            playerSprite.scale(resMultiplier, resMultiplier);
+            moveTarget = sf::Vector2f(playerSprite.getPosition());
         }
 
-        // walk left
-        if (direction.x < 0 && positiveDirectionX > positiveDirectionY){
-            // TODO - use iterateSprite()
-            playerMask.top = 80;
-            if (clock.getElapsedTime().asSeconds() > 0.08f){
-                if (playerMask.left >= 192) {
-                    playerMask.left = 0;
+        // Cycle through animation frames
+        void iterateSprite(int top, int startLeft, int maxLeft, int incrementLeft, float speed){
+            playerMask.top = top;
+            if (clock.getElapsedTime().asSeconds() > speed){
+                if (playerMask.left >= maxLeft) {
+                    playerMask.left = startLeft;
                 }
                 else {
-                    playerMask.left += 64;
+                    playerMask.left += incrementLeft;
                 }
                 playerSprite.setTextureRect(playerMask);
                 clock.restart();
+                //playerTexture.update(window);
             }
         }
-            // walk right
-        else if (direction.x > 0 && positiveDirectionX > positiveDirectionY){
-            // TODO - use iterateSprite()
-            playerMask.top = 160;
-            if (clock.getElapsedTime().asSeconds() > 0.08f) {
-                if (playerMask.left >= 192) {
-                    playerMask.left = 0;
-                } else {
-                    playerMask.left += 64;
-                }
-                playerSprite.setTextureRect(playerMask);
-                clock.restart();
+
+        void setTarget(){
+            moveTarget = sf::Vector2f(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+        }
+
+        void walk(){
+            sf::Vector2f direction = sf::Vector2f(moveTarget.x, moveTarget.y) - playerSprite.getPosition();
+            float magnitude = sqrt((direction.x * direction.x) + (direction.y * direction.y));
+            sf::Vector2f unitVector((direction.x * 1.3f) / magnitude, direction.y / (magnitude * 1.8f));
+
+            // overall player-speed
+            playerSprite.move(unitVector * 0.8f * resMultiplierF);
+
+            // direction stuff
+            float positiveDirectionX;
+            float positiveDirectionY;
+
+            if (direction.x < 0){
+                positiveDirectionX = - direction.x;
+            } else {
+                positiveDirectionX = direction.x;
+            }
+            if (direction.y < 0){
+                positiveDirectionY = - direction.y;
+            } else {
+                positiveDirectionY = direction.y;
+            }
+
+                // walk left
+            if (direction.x < 0 && positiveDirectionX > positiveDirectionY){
+                iterateSprite(80, 0, 192, 64, .08f);
+            }
+                // walk right
+            else if (direction.x > 0 && positiveDirectionX > positiveDirectionY){
+                iterateSprite(160, 0, 192, 64, .08f);
+            }
+                // walk front
+            else if (positiveDirectionX < positiveDirectionY && direction.y > 0){
+                iterateSprite(240, 0, 320, 64, .08f);
+            }
+                // walk back
+            else if (positiveDirectionX < positiveDirectionY && direction.y < 0){
+                iterateSprite(320, 0, 320, 64, .08f);
             }
         }
-            // walk front
-        else if (positiveDirectionX < positiveDirectionY && direction.y > 0){
-            // TODO - use iterateSprite()
-            playerMask.top = 240;
-            if (clock.getElapsedTime().asSeconds() > 0.08f) {
-                if (playerMask.left >= 320) {
-                    playerMask.left = 0;
-                } else {
-                    playerMask.left += 64;
-                }
-                playerSprite.setTextureRect(playerMask);
-                clock.restart();
+
+        void idle(){
+            iterateSprite(0, 0, 448, 64, .2f);
+        }
+
+        void animate(){
+            if (round(moveTarget.x) != round(playerSprite.getPosition().x) && round(moveTarget.y) != round(playerSprite.getPosition().y)){
+                walk();
+            } else {
+                idle();
             }
         }
-            // walk back
-        else if (positiveDirectionX < positiveDirectionY && direction.y < 0){
-            // TODO - use iterateSprite()
-            playerMask.top = 320;
-            if (clock.getElapsedTime().asSeconds() > 0.08f) {
-                if (playerMask.left >= 320) {
-                    playerMask.left = 0;
-                } else {
-                    playerMask.left += 64;
+
+        int getPositionX(){
+            float posF = playerSprite.getPosition().x;
+            int posI = floor((posF * 100.0) + 0.5) / 100;
+            return posI;
+        }
+    };
+
+    class Room{
+    private:
+        sf::Texture roomTexture;
+        bool isScrollable;
+
+    public:
+        sf::View view;
+        sf::FloatRect viewMask;
+        sf::Sprite roomSprite;
+        float moveTargetX;
+        // constructor
+        explicit Room(const string &filePath){
+            viewMask.left = 0;
+            viewMask.top = 0;
+            viewMask.width = 320 * resMultiplier;
+            viewMask.height = 200 * resMultiplier;
+            view.reset(viewMask);
+            roomTexture.loadFromFile(filePath);
+            roomSprite.setTexture(roomTexture);
+            roomSprite.scale(resMultiplier, resMultiplier);
+            //cout << "size is " << roomTexture.getSize().x << endl;
+            if (roomTexture.getSize().x != viewMask.width || roomTexture.getSize().y != viewMask.height){
+                isScrollable = true;
+            } else {
+                isScrollable = false;
+            }
+        }
+
+        void scrollHorizontal(float playerPositionX){
+            if (isScrollable){
+                if (playerPositionX > viewMask.width + viewMask.left - 100 * resMultiplier && roomTexture.getSize().x * resMultiplier > viewMask.width + viewMask.left){
+                    viewMask.left += 1 * resMultiplier;
+                } else if (playerPositionX < viewMask.left + 100 * resMultiplier && viewMask.left > 0){
+                    viewMask.left -= 1 * resMultiplier;
                 }
-                playerSprite.setTextureRect(playerMask);
-                clock.restart();
             }
         }
     };
-    void animate(){
-        if (round(moveTarget.x) != round(playerSprite.getPosition().x) && round(moveTarget.y) != round(playerSprite.getPosition().y)){
-            walk();
-        } else {
-            idle();
-        }
-    }
-    int getPositionX(){
-        float posF = playerSprite.getPosition().x;
-        int posI = floor((posF * 100.0) + 0.5) / 100;
-        return posI;
-    }
-};
 
-class Room{
-private:
-    sf::Texture roomTexture;
-    bool isScrollable;
+    class Item{
+    private:
+        sf::Texture texture;
 
-public:
-    sf::View view;
-    sf::FloatRect viewMask;
-    sf::Sprite roomSprite;
-    float moveTargetX;
-    // constructor
-    explicit Room(const string &filePath){
-        viewMask.left = 0;
-        viewMask.top = 0;
-        viewMask.width = 320 * resMultiplier;
-        viewMask.height = 200 * resMultiplier;
-        view.reset(viewMask);
-        roomTexture.loadFromFile(filePath);
-        roomSprite.setTexture(roomTexture);
-        roomSprite.scale(resMultiplier, resMultiplier);
-        //cout << "size is " << roomTexture.getSize().x << endl;
-        if (roomTexture.getSize().x != viewMask.width || roomTexture.getSize().y != viewMask.height){
-            isScrollable = true;
-        } else {
-            isScrollable = false;
-        }
-    }
+    public:
+        sf::Sprite sprite;
 
-    void scrollHorizontal(float playerPositionX){
-        if (isScrollable){
-            if (playerPositionX > viewMask.width + viewMask.left - 100 * resMultiplier && roomTexture.getSize().x * resMultiplier > viewMask.width + viewMask.left){
-                viewMask.left += 1 * resMultiplier;
-            } else if (playerPositionX < viewMask.left + 100 * resMultiplier && viewMask.left > 0){
-                viewMask.left -= 1 * resMultiplier;
-            }
+        Item(const string filePath, float positionX, float positionY){
+            texture.loadFromFile(filePath);
+            sprite.setTexture(texture);
+            sprite.scale(resMultiplier, resMultiplier);
+            sprite.setPosition(positionX, positionY);
         }
-    }
-};
+    };
 }
 
 int main() {
@@ -232,6 +201,7 @@ int main() {
     window.setMouseCursorVisible(false);
 
     // Frame Clock
+
     sf::Clock clock;
     sf::Clock moveClock;
 
@@ -243,6 +213,8 @@ int main() {
 
     BRO::Room currentRoom = arcade;
     BRO::Player currentPlayer = player;
+
+    BRO::Item test("cursor.png", 500, 500);
 
     while(window.isOpen()){
         while(window.pollEvent(event)) {
@@ -273,6 +245,7 @@ int main() {
         //window.draw(bedroom.roomSprite);
         //window.draw(shop.roomSprite);
         window.draw(currentRoom.roomSprite);
+        window.draw(test.sprite);
         window.draw(currentPlayer.playerSprite);
         window.draw(cursor.cursorSprite);
 
